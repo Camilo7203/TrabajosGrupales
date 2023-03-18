e88719421ddd7f56b5d58776473025e02c6fbd42
dacaicedoe <126344550+dacaicedoe@users.noreply.github.com>
2023-03-18 18:28:51 -0500

    Add files via upload

---
+++ Actividad2/Parte1/c4e22.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class c4e22 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double valor, horas, salarioMensual;
+        String nombre;
+
+        System.out.println("Ingrese el nombre:");
+        nombre = input.nextLine();
+
+        System.out.println("Ingrese el salario por hora de "+ nombre);
+        valor = input.nextDouble();
+
+        System.out.println("Ingrese el numero de horas trabajadas");
+        horas = input.nextDouble();
+
+
+        salarioMensual = valor*horas;
+
+        if (salarioMensual>450000) {
+            System.out.println("Nombre: "+nombre+". Salario: "+salarioMensual);
+        }
+        else {
+            System.out.println("Nombre: "+nombre);
+        }
+    }
+}
