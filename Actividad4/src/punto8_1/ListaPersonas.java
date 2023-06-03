9a7f9f37b103cbd428b30a9877cb4d0f4ef7d6c2
sanbedoyau <126343649+sanbedoyau@users.noreply.github.com>
2023-06-03 17:38:48 -0500

    Actividad 4

--- Actividad4/src/punto8_1/ListaPersonas.java
+++ Actividad4/src/punto8_1/ListaPersonas.java
@@ -3,5 +3,18 @@ package punto8_1;
 import java.util.Vector;
 
 public class ListaPersonas {
-    
+    Vector listaPersonas;
+
+    public ListaPersonas() {
+        listaPersonas = new Vector();
+    }
+    public void añadirPersona(punto8_1.Persona p) {
+        listaPersonas.add(p);
+    }
+    public void eliminarPersona(int i) {
+        listaPersonas.removeElementAt(i);
+    }
+    public void borrarLista() {
+        listaPersonas.removeAllElements();
+    }
 }
